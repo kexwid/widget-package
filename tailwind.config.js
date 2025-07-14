40c6e4700cada42387766ccfcc0881fbcf2a1487
0xGecko <0xgecko.dev@gmail.com>
2025-07-14 15:07:25 +0200

    added build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,7 @@
+// tailwind.config.mjs
+import daisyui from 'daisyui';
+import themes from 'daisyui/src/theming/themes';
+
 /** @type {import('tailwindcss').Config} */
 export default {
     darkMode: ['class'],
@@ -9,22 +13,18 @@ export default {
     theme: {
         extend: {},
     },
-    plugins: [require('daisyui')],
+    plugins: [daisyui],
     daisyui: {
         themes: [
             {
                 light: {
-                    ...require('daisyui/src/theming/themes')[
-                        '[data-theme=light]'
-                    ],
+                    ...themes['[data-theme=light]'],
                     primary: '#666cff',
                 },
             },
             {
                 dark: {
-                    ...require('daisyui/src/theming/themes')[
-                        '[data-theme=dark]'
-                    ],
+                    ...themes['[data-theme=dark]'],
                     primary: '#666cff',
                     'base-100': '#2a334c',
                 },
